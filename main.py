8ddf875a0c11238d9888806aad5a72cac6bc4608
nocturnal <foxnezz@gmail.com>
2019-04-17 00:25:23 +0300

    added backprop

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ def pr(x):
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
+def sigmoid_prime(x):
+    return sigmoid(x) * (1 - sigmoid(x))
+
 def generate_data(n):
     a = np.random.randn(n, 1)
     b = np.random.randn(n, 1)
@@ -65,6 +68,38 @@ class Network:
         
         self.weights = [w - (eta / len(mini_batch)) * nw for w, nw in zip(self.weights, nabla_w)]
         self.biases = [b - (eta / len(mini_batch)) * nb for b, nb in zip(self.biases, nabla_b)]
+    
+    def backprop(self, x, y):
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+
+        activation = x
+        activations = [x]
+        zs = []
+        for b, w in zip(self.biases, self.weights):
+            z = np.dot(w, activation)+b
+            zs.append(z)
+            activation = sigmoid(z)
+            activations.append(activation)
+        # backward pass
+        delta = self.cost_derivative(activations[-1], y) * sigmoid_prime(zs[-1])
+        nabla_b[-1] = delta
+        nabla_w[-1] = np.dot(delta, activations[-2].transpose())
+        
+        for l in range(2, self.layer_count):
+            z = zs[-l]
+            sp = sigmoid_prime(z)
+            delta = np.dot(self.weights[-l+1].transpose(), delta) * sp
+            nabla_b[-l] = delta
+            nabla_w[-l] = np.dot(delta, activations[-l-1].transpose())
+        return (nabla_b, nabla_w)
+    
+    def evaluate(self, test_data):
+        test_results = [(np.argmax(self.feedforward(x)), y) for (x, y) in test_data]
+        return sum(int(x == y) for (x, y) in test_results)
+
+    def cost_derivative(self, output_activations, y):
+        return (output_activations - y)
 
 def main():
     data = generate_data(10)
