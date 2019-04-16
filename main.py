8255cbfab51541f3c8853cafcf9308ea3772e6c8
nocturnal <foxnezz@gmail.com>
2019-04-16 23:04:12 +0300

    refactored network

--- main.py
+++ main.py
@@ -26,49 +26,20 @@ def generate_data(n):
 
     return {'x': X, 'y': Y}
 
-# class Neuron:
-#     def __init__(self, layer = None, weights = None, bias = None, calculated = None):
-#         self.layer = layer
-#         self.weights = weights
-#         self.bias = bias
-#         self.calculated = calculated
-    
-#     def calculate(self):
-#         if self.calculated != None:
-#             return self.calculated
-        
-#         layer = np.array([a.calculate() for a in self.layer])
-#         return sigmoid(np.dot(layer, self.weights) + self.bias)
-
-class Layer:
-    def __init__(self, previous_count, count):
-        self.previous_count = previous_count
-        self.count = count
-
-        self.weights = np.random.randn(count, previous_count)
-        self.biases = np.random.randn(count, 1)
-    
-    def calculate(self, previous):
-        return activation(np.matmul(self.weights, previous) + self.biases)
-
 class Network:
     def __init__(self, architecture):
-        self.layers = []
-        for i in range(len(architecture) - 1):
-            self.layers.append(Layer(architecture[i], architecture[i + 1]))
+        self.architecture = architecture
+        self.layer_count = len(architecture)
+        self.weights = [np.random.randn(count, previous_count) for previous_count, count in zip(self.architecture[:-1], self.architecture[1:])]
+        self.biases = [np.random.randn(count, 1) for count in self.architecture[1:]]
         
-    def feedforward(self, X):
-        current = X.reshape(-1, 1)
-
-        for layer in self.layers:
-            current = layer.calculate(current)
-        
-        return current
+    def feedforward(self, a):
+        for w, b in zip(self.weights, self.biases):
+            a = activation(np.dot(w, a) + b)
+        return a
     
     def cost(self, X, realY):
         Y = self.feedforward(X)
-        print('Y: {}, realY: {}'.format(Y, realY))
-        pr(Y - realY)
         return ((Y - realY) ** 2).sum()
 
 def main():
@@ -81,7 +52,11 @@ def main():
     # Y:   o
 
     network = Network([3, 3, 3, 1])
-    costs = [network.cost(data['x'][i], np.array([[data['y'][i]]])) for i in range(len(data['x']))]
-    pr(costs)
+
+    for i in range(len(data['x'])):
+        X = np.array(data['x'][i]).reshape(-1, 1)
+        Y = np.array(data['y'][i]).reshape(-1, 1)
+        cost = network.cost(X, Y)
+        pr(cost)
 
 main()
