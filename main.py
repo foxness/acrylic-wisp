68ae31aaa85e08bd2706a49fdaaa45ab7755247d
nocturnal <foxnezz@gmail.com>
2019-04-16 20:13:08 +0300

    added training data generation

--- main.py
+++ main.py
@@ -1,3 +1,35 @@
-import numpy
+import numpy as np
+import random
 
-print('hello world')
+np.random.seed(1448)
+
+def pr(x):
+    print(repr(x))
+
+def generate_data(n):
+    a = (np.random.rand(n, 1) - 0.5) * 100
+    b = (np.random.rand(n, 1) - 0.5) * 100
+    c = (np.random.rand(n, 1) - 0.5) * 100
+
+    X = np.hstack((a, b, c))
+    Y = np.array([x[0] + x[1] * x[2] for x in X])
+
+    return {'x': X, 'y': Y}
+
+# network architecture
+# X: o o o
+#    o o o
+#    o o o
+# Y:   o
+
+# class Network:
+#     def __init__(self):
+#         pass
+    
+#     def calculate_neuron(a, w, b):
+        
+
+def main():
+    pr(generate_data(10))
+
+main()
